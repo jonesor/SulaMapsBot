27efebcfe343924625165ea88300792f402ffa9d
Owen Jones <jonesor@gmail.com>
2022-03-16 13:30:07 +0100

    Update SulaMapsBot.R

--- SulaMapsBot.R
+++ SulaMapsBot.R
@@ -11,7 +11,8 @@ SulaMapsBot_token <- rtweet::create_token(
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
 )
 
 # Hash tags ----
